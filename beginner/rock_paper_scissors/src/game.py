3830fb0e2633fff36e2f35f493eb4bb397c6c8c4
Banafsheh Khazali <banafshehkhazali@Mac.lan>
2025-09-09 12:39:52 -0600

    docstring added

--- beginner/rock_paper_scissors/src/game.py
+++ beginner/rock_paper_scissors/src/game.py
@@ -1,22 +1,35 @@
+from ast import Str
 import random
+from typing import List
 
 
 class RockPaperScissors:
-    def __init__(self, name):
-        self.player_name = name
-        self.choices = ["rock", "paper", "scissors"]
+    """Main class for the Rock Paper Scissors game"""
+    def __init__(self, name: str):
+        self.player_name: str = name
+        self.choices: List[str] = ["rock", "paper", "scissors"]
 
     def get_player_choice(self):
-        user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+        user_choice: str = input("Enter your choice (rock, paper, scissors): ").lower()
         if user_choice not in self.choices:
             print("Invalid choice. Please try again.")
             return self.get_player_choice()
         return user_choice.lower()
 
     def get_computer_choice(self):
+        """Get the computer's choice randomly"""
         return random.choice(self.choices)
 
-    def decide_winner(self, computer_choice, user_choice):
+    def decide_winner(self, computer_choice: str, user_choice: str) -> str:
+        """Decide the winner of the game
+        
+        Args:
+            computer_choice: The computer's choice
+            user_choice: The user's choice
+
+        Returns:
+            The winner of the game
+        """
         if computer_choice == user_choice:
             return "It's a tie!"
         elif computer_choice == "rock" and user_choice == "scissors":
